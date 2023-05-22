771238d11053d843486b42e05b347b35990ec717
Nikita <s2015836@x.donstu.ru>
2023-05-22 23:11:30 +0300

    add Footer

---
+++ src/widgets/footer/ui.tsx
@@ -0,0 +1,9 @@
+import classes from './style.module.scss';
+
+export function Footer() {
+  return (
+    <footer className={classes.footer}>
+      Make with ❤️ for the MobProgramming team
+    </footer>
+  );
+}
