862d3a8fd62fa512fe41c9a1ede75921c28dd909
Nikita <s2015836@x.donstu.ru>
2023-05-24 18:37:14 +0300

    add active state to link

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 import { ContentContainer } from '@/shared/ui';
 
 import { routes } from './confing';
@@ -9,6 +13,7 @@ import classes from './style.module.scss';
 import logo from './assets/logo.png';
 
 export function Header() {
+  const pathname = usePathname();
   return (
     <header className={classes.header}>
       <ContentContainer className={classes.header__container}>
@@ -19,7 +24,12 @@ export function Header() {
           <ul className={classes.header__links}>
             {routes.map((route) => (
               <li key={route.id}>
-                <Link href={route.href} className={classes.header__link}>
+                <Link
+                  href={route.href}
+                  className={clsx(classes.header__link, {
+                    [classes.header__link_active]: pathname.startsWith(route.href),
+                  })}
+                >
                   {route.label}
                 </Link>
               </li>
